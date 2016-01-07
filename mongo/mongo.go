c1dc6bb672e685ca37556f43445bca2957662b18
Paul Fortin <fortinp1@uoiitpaulfortin.urbanout.com>
2016-01-07 15:39:17 -0500

    Final Commit

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,14 +1,14 @@
+// mongo - CRUD package for the Profile API micro-service
 package mongo
 
 import (
 	"time"
 	"log"
-	"encoding/base64"
-
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"encoding/base64"
 
-	"ProfileService/crypto"
+	"ProfileService-IdiomaticGo/crypto"
 )
 
 // Profile - is the memory representation of one user profile
@@ -16,9 +16,8 @@ type Profile struct {
 	Name 			string			`json: "username"`
 	Password 		string			`json: "password"`
 	Age 			int				`json: "age"`
-	LastUpdated     time.Time		`json: "last_updated"`
+	LastUpdated     time.Time
 }
-
 
 func init() {
 	tmp, _ := crypto.Encrypt([]byte( "test1234"))
@@ -34,6 +33,7 @@ func init() {
 	p1.CreateOrUpdateProfile()
 }
 
+// GetProfiles - Returns all the profile in the Profiles Collection
 func GetProfiles() []Profile {
 	session, err := mgo.Dial("localhost:27017")
 	if err != nil {
@@ -52,6 +52,44 @@ func GetProfiles() []Profile {
 	return profiles
 }
 
+
+// ShowProfile - Returns the profile in the Profiles Collection with name equal to the id parameter (id == name)
+func ShowProfile(id string) Profile {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Println("Could not connect to mongo: ", err.Error())
+		return Profile{}
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("ProfileService").C("Profiles")
+	profile := Profile{}
+	err = c.Find(bson.M{"name": id}).One(&profile)
+
+	return profile
+}
+
+
+// DeleteProfile - Deletes the profile in the Profiles Collection with name equal to the id parameter (id == name)
+func DeleteProfile(id string) bool {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Println("Could not connect to mongo: ", err.Error())
+		return false
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("ProfileService").C("Profiles")
+	err = c.RemoveId(id)
+
+	return true
+}
 
 
 // CreateOrUpdateProfile - Creates or Updates (Upsert) the profile in the Profiles Collection with id parameter
@@ -74,3 +112,4 @@ func (p *Profile) CreateOrUpdateProfile() bool {
 	}
 	return true
 }
+
--- routes/router.go
+++ routes/router.go
@@ -1,15 +1,20 @@
+// routes - Routing package for the Profile API micro-service
 package routes
 
 import (
-	"log"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+	"github.com/gorilla/mux"
 	"encoding/json"
 
-	"github.com/gorilla/mux"
+	"encoding/base64"
 
-	"ProfileService/mongo"
+	"ProfileService-IdiomaticGo/mongo"
+	"ProfileService-IdiomaticGo/crypto"
 )
+
 
 func Start() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -22,8 +27,10 @@ func Start() {
 	//v2 API calls
 	router.HandleFunc("/v2/profile", v2HandleProfileNoParams).Methods("GET")
 
+	log.Println("Starting Profile Service on port 8080")
 	log.Fatal(http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", router))
 }
+
 
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Println("Index Request")
@@ -59,21 +66,81 @@ func getProfile(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(mongo.GetProfiles())
 }
 
+
 //createProfile - Create a profile in the DB
 func createProfile(w http.ResponseWriter, r *http.Request) {
+	log.Println("Create Profile")
+	var resp struct{}
+
+	p := mongo.Profile{}
+	decoder := json.NewDecoder(r.Body)
+	error := decoder.Decode(&p)
+	if error != nil {
+		log.Println(error.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+	tmp, _ := crypto.Encrypt([]byte(p.Password))
+	p.Password = base64.StdEncoding.EncodeToString(tmp)
+	p.LastUpdated = time.Now()
+	p.CreateOrUpdateProfile()
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 //updateProfile - Update a profile in the DB
 func updateProfile(w http.ResponseWriter, r *http.Request) {
+	log.Println("Create Profile")
+	var resp struct{}
+
+	p := mongo.Profile{}
+	decoder := json.NewDecoder(r.Body)
+	error := decoder.Decode(&p)
+	if error != nil {
+		log.Println(error.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+	tmp, _ := crypto.Encrypt([]byte(p.Password))
+	p.Password = base64.StdEncoding.EncodeToString(tmp)
+	p.LastUpdated = time.Now()
+	p.CreateOrUpdateProfile()
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 //showProfile - given the profile ID in the request this returns the profile to the caller
 func showProfile(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	profileID := vars["profileId"]
+	log.Println("Show Profile: ", profileID)
+	repl := mongo.ShowProfile(profileID)
+	if repl.Name == "" {
+		var repl struct{}
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(repl)
+		return
+	}
+	json.NewEncoder(w).Encode(repl)
 }
 
 // deleteProfile - Removes a profile from the DB
 func deleteProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var repl struct{}
+	vars := mux.Vars(r)
+	profileID := vars["profileId"]
+	log.Println("Delete Profile: ", profileID)
+	mongo.DeleteProfile(profileID)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(repl)
 }
 
 
@@ -87,4 +154,9 @@ func v2HandleProfileNoParams(w http.ResponseWriter, r *http.Request) {
 
 
 func v2GetProfile(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get All Profiles")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//json.NewEncoder(w).Encode(Profiles)
+	json.NewEncoder(w).Encode(mongo.GetProfiles())
 }
